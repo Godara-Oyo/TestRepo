28912bb37ae64d770fdb8cf5c51794f50e3cca1d
varunudemy5 <47503848+varunudemy5@users.noreply.github.com>
2019-09-28 15:59:54 +0530

    create test.py

---
+++ test.py
@@ -0,0 +1,21 @@
+import logging
+import random
+
+print ("hello");
+
+
+def isEven():
+    current_number = random.randint(1,100)
+    print (current_number)
+    if current_number % 2 != 0:
+        return "odd"
+    else:
+        return "even"
+
+wasiteven = isEven()
+
+print(wasiteven)
+print(wasiteven)
+print(wasiteven)
+
+
